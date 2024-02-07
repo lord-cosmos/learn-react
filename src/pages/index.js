718bc99dac38ff76fc6987fa7d372c356897fec5
lord-cosmos <teslaatozinspires7@gmail.com>
2024-02-07 15:24:51 -0500

    fix bios.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,12 @@ import styles from "@/styles/Home.module.css";
 // import Profile from './components/profile'
 // import Profile from "./qcomps/profile_mistake";
 import MyComp from "./qcomps/firstcomp";
+import Bio from "./qcomps/bios";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <MyComp />
+      <Bio />
     </div>
   );
 }
