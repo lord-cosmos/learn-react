cae3abc64bb621dcac0b19c0b0ac648b6499f440
lord-cosmos <teslaatozinspires7@gmail.com>
2024-02-07 16:10:47 -0500

    fix qcomps/list_keys_id.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,13 @@ import Bio from "./qcomps/bios";
 import TodoList from "./qcomps/todos";
 import Profile from "./components/profile_props";
 import Gallery from "./qcomps/gallery_props";
-import PackingList from "./qcomps/props_item";
+// import PackingList from "./qcomps/props_item";
+import List from "./qcomps/list_keys_id";
+
 export default function Home() {
   return (
     <div className={styles.main}>
-      <Gallery />
-      <PackingList />
-      <Profile />
-      <Bio />
-      <TodoList />
+      <List />
     </div>
   );
 }
