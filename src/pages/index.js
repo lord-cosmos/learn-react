4328274cf9e1e88a660c465338d3eed5b24a33bb
lord-cosmos <teslaatozinspires7@gmail.com>
2024-02-14 15:38:52 -0500

    fix bug in artistsRemoveArr - using splice is bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import styles from "@/styles/Home.module.css";
-
-import Scoreboard from "./qcomps/updObjectsForm";
+import List from "./qcomps/artistsRemoveArr";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <Scoreboard />
+      <List />
     </div>
   );
 }
